046902f6f8950cbe9b8ec68cf72d6a14549c5ea9
paytonshafer <paytonshafer@gmail.com>
2023-11-11 16:28:36 -0500

    realworld flyweight

---
+++ Structural-Patterns/Flyweight/car-db.ts
@@ -0,0 +1,97 @@
+// Flyweight
+class Flyweight {
+    private sharedState: any;
+
+    constructor(sharedState: any) {
+        this.sharedState = sharedState;
+    }
+
+    public operation(uniqueState): void {
+        const s = JSON.stringify(this.sharedState);
+        const u = JSON.stringify(uniqueState);
+        console.log(`Flyweight: Displaying shared (${s}) and unique (${u}) state.`);
+    }
+}
+
+// FlyweightFactory
+class FlyweightFactory {
+    private flyweights: {[key: string]: Flyweight} = <any>{};
+
+    constructor(initialFlyweights: string[][]) {
+        for (const state of initialFlyweights) {
+            this.flyweights[this.getKey(state)] = new Flyweight(state);
+        }
+    }
+
+    private getKey(state: string[]): string {
+        return state.join('_');
+    }
+
+    public getFlyweight(sharedState: string[]): Flyweight {
+        const key = this.getKey(sharedState);
+
+        if (!(key in this.flyweights)) {
+            console.log('FlyweightFactory: Can\'t find a flyweight, creating new one.');
+            this.flyweights[key] = new Flyweight(sharedState);
+        } else {
+            console.log('FlyweightFactory: Reusing existing flyweight.');
+        }
+
+        return this.flyweights[key];
+    }
+
+    public listFlyweights(): void {
+        const count = Object.keys(this.flyweights).length;
+        console.log(`\nFlyweightFactory: I have ${count} flyweights:`);
+        for (const key in this.flyweights) {
+            console.log(key);
+        }
+    }
+}
+
+// Client code starts with defaulkt cars in db
+const cars = new FlyweightFactory([
+    ['Chevrolet', 'Camaro2018', 'pink'],
+    ['Mercedes Benz', 'C300', 'black'],
+    ['Mercedes Benz', 'C500', 'red'],
+    ['BMW', 'M5', 'red'],
+    ['BMW', 'X6', 'white'],
+]);
+cars.listFlyweights();
+
+function addCarToDatabase(ff: FlyweightFactory, plates: string, owner: string, brand: string, model: string, color: string) {
+    console.log('\nClient: Adding a car to database.');
+    const flyweight = ff.getFlyweight([brand, model, color]);
+    flyweight.operation([plates, owner]);
+}
+
+addCarToDatabase(cars, 'CL234IR', 'Payton', 'BMW', 'M5', 'red');
+
+addCarToDatabase(cars, 'CL234IR', 'Chloe', 'BMW', 'X1', 'red');
+
+cars.listFlyweights();
+
+/* OUTPUT
+FlyweightFactory: I have 5 flyweights:
+Chevrolet_Camaro2018_pink
+Mercedes Benz_C300_black
+Mercedes Benz_C500_red
+BMW_M5_red
+BMW_X6_white
+
+Client: Adding a car to database.
+FlyweightFactory: Reusing existing flyweight.
+Flyweight: Displaying shared (["BMW","M5","red"]) and unique (["CL234IR","Payton"]) state.
+
+Client: Adding a car to database.
+FlyweightFactory: Can't find a flyweight, creating new one.
+Flyweight: Displaying shared (["BMW","X1","red"]) and unique (["CL234IR","Chloe"]) state.
+
+FlyweightFactory: I have 6 flyweights:
+Chevrolet_Camaro2018_pink
+Mercedes Benz_C300_black
+Mercedes Benz_C500_red
+BMW_M5_red
+BMW_X6_white
+BMW_X1_red
+*/
